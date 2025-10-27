40900cf5f12641d998ba6bb0f0fe862591cee07b
vignesh <137668196+vignesh-04rep@users.noreply.github.com>
2025-10-27 15:49:21 +0530

    Refactor UART handling and improve data display
    Refactor UART service routine and update UART configuration.

--- UART_LCD/uart_test.c
+++ UART_LCD/uart_test.c
@@ -15,33 +15,6 @@ void uart_data_tx(unsigned char inputer)
 	while((U1LSR & (1<<5) )== 0);
 	U1THR=inputer;
 }
-void uart_service_routine(void)__irq//irq keyword
-{
-	unsigned char data;
-	if(U0IIR&0x04)
-	{
-		data=U0RBR;
-		data_display(data);
-		uart_data_tx(data);
-		if(data=='y')
-		{
-			
-			IOSET0=0xFF;
-			
-			
-			
-		}
-		if(data=='n')
-		{
-			IOCLR0=0xFF;
-			wait(100);
-			
-		}
-	}
-	VICVectAddr=0;
-}
-
-
 char uart_data_rx(void)
 {
 	unsigned char rec;
@@ -50,6 +23,43 @@ char uart_data_rx(void)
 	return rec;
 	
 }
+void uart_service_routine(void)__irq
+{
+	unsigned char data;
+	unsigned int looper=0;
+	
+	
+	if((U1IIR&0x0E)==0x04)
+	{
+		data=uart_data_rx(); 
+		looper++;
+		 data = U1RBR;
+		 
+     wait(2);
+		if(looper<8)
+		{
+			lcd_cmd(0x80+looper);
+     data_display(data);
+			uart_data_tx(data);
+		}
+		else
+		{
+			lcd_cmd(0xc0+(looper-8));
+			data_display(data);
+			if(looper==16)
+			{
+				looper=0;
+			}
+		}
+        
+		
+	}
+	VICVectAddr=0;
+}
+	
+
+
+
 void uart_config(void)
 {
 	PINSEL0 &= ~(0xF00);              
@@ -57,23 +67,22 @@ void uart_config(void)
 
 	
 	U1LCR=0x83;
-	U1DLL=97;
+	U1DLL=78;
 	U1DLM=0;
 	U1LCR=0x03;
 	
-	//U0IER=1<<0;
+	U1IER=1<<0;
 	
 	VICIntSelect=0;
-	VICVectCntl4=0x20|6;
+	VICVectCntl4=0x20|7;
 	VICVectAddr4=(unsigned long)uart_service_routine;
-	VICIntEnable=1<<6;
+	VICIntEnable=1<<7;
 	
 	
 	
 }
 int main()
 {
-		unsigned char buffer=0;
 		lcd_config();
 		uart_config();
 		
@@ -82,14 +91,8 @@ int main()
 		
 	while(1)
 	{
-		wait(100);
-		buffer=uart_data_rx();
-		lcd_cmd(0x80);
-		uart_data_tx(buffer);
-		data_display(buffer);
-		wait(100);
 		
-		
+		while(1);
 	}
 }
 
